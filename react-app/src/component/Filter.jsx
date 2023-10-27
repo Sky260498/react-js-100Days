d2e3fe564218a34b0580112c459500cdee8db3cd
Akash SB <asb260498@gmail.com>
2023-10-27 17:33:27 +0530

    Added Additional features

--- react-app/src/component/Filter.jsx
+++ react-app/src/component/Filter.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Earth from '../icons/Earth'
 
-function Filter({filterCountryList}) {
+function Filter({filterCountryList, addSearchTerm, searchedData}) {
+  const [inputVal, setInputVal] = useState('');
+  const handleKeyEvent =(e)=> {
+    if(e.key == " " ||
+    e.code == "Space" ||      
+    e.keyCode == 32 || e.keyCode == 13) {
+      addSearchTerm(e.target.value);     
+      setInputVal("");
+      filterCountryList("");
+    }
+  }
+
+  const handleChangeEvent =(e)=> {
+    setInputVal(e.target.value);
+    filterCountryList(e.target.value);
+  }
+
   return (
     <div className="main-container">
       <div className='mx-auto w-1/2 p-2'>
-      <div className="relative mt-2 rounded-md shadow-sm">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <span className="text-gray-500 sm:text-sm"><Earth /></span>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <div>
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="text-gray-500 sm:text-sm"><Earth /></span>
+            </div>        
+            <input
+              type="text"
+              name="country"
+              id="country"
+              value={inputVal}
+              onChange={(e)=>handleChangeEvent(e)}
+              onKeyUp={(e)=>handleKeyEvent(e)}
+              className="block w-full rounded-md border-0 py-3 pl-12 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-200 sm:text-sm sm:leading-6"
+              placeholder="Search For Country"
+            />
+          </div>        
         </div>
-        <input
-          type="text"
-          name="country"
-          id="country"
-          onChange={(e)=>filterCountryList(e.target.value)}
-          className="block w-full rounded-md border-0 py-3 pl-12 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-200 sm:text-sm sm:leading-6"
-          placeholder="Search For Country"
-        />
-      </div>
-
+        <div>
+          {
+            searchedData.length > 0 && <div className='rounded-md px-2 pt-2 flex flex-wrap w-full border-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-200 sm:text-sm sm:leading-6'>
+            {searchedData.map((searchTerm)=><div key={searchTerm} className='capitalize rounded-md px-2 mx-1 mb-2 ring-1 ring-inset ring-gray-300'>{searchTerm}</div>)}
+          </div>
+          }
+        </div>
       </div>
     </div>
   )
--- react-app/src/module/FilterModule.jsx
+++ react-app/src/module/FilterModule.jsx
@@ -5,19 +5,33 @@ import countries from '../data/countryList';
 
 function FilterModule() {    
       const [countryList, setCountryList] = useState(countries);
+      const [searchArray, setSearchArray] = useState([]);
     
       const filterCountryList = (searchTerm) => {
         if(searchTerm == "") {
-          return;
+            setCountryList(countries);
         } else {
           const filteredCountries = countries.filter((country) =>  country.toLowerCase().includes(searchTerm.toLowerCase()));
           setCountryList(filteredCountries);
         }
       }
+
+      const addSearchTerm = (addedTerm) => {
+        countries.forEach((country) => {
+            if(country.toLowerCase().includes(addedTerm.toLowerCase()) && !searchArray.includes(addedTerm.toLowerCase())) {
+                setSearchArray([...searchArray, addedTerm.toLowerCase()]);
+            }
+        });        
+      }
+
+      const removeSearchTerm = (removedTerm) => {
+        searchArray.filter((term) => !term.includes(removedTerm));
+      }
+
   return (
     <div>
-        <Filter filterCountryList={filterCountryList} />
-      <CountryList countries={countryList} />
+        <Filter filterCountryList={filterCountryList} addSearchTerm={addSearchTerm} removeSearchTerm={removeSearchTerm} searchedData={searchArray} />
+        <CountryList countries={countryList} />
     </div>
   )
 }
